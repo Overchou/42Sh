28a2efcbf708c7a01e3b50b0e0695ade041498a4
auffra_a <auffra_a@epitech.eu>
2014-05-25 00:23:29 +0200

    cd newww

--- cd_by_booba/cd.c
+++ cd_by_booba/cd.c
@@ -5,7 +5,7 @@
 ** Login   <auffra_a@epitech.net>
 ** 
 ** Started on  Thu May 22 18:11:39 2014 auffra_a
-** Last update Fri May 23 16:49:34 2014 auffra_a
+** Last update Sun May 25 00:23:16 2014 auffra_a
 */
 
 #include <unistd.h>
@@ -45,10 +45,10 @@ t_env	*cd_minus(t_env *env)
       if ((my_nncmp(tmp->value, "OLDPWD=", 0)) == 0)
 	{
 	  pwd = my_ncpy(tmp->value, 7);
+	  my_printf("%s\n", pwd);
 	  if ((chdir(pwd)) != 0)
 	    return (NULL);
 	  pwd = my_concat("PWD ", pwd);
-	  my_printf("\n%s\n%s\n", pwd, oldpwd);
 	  //	  env = setenv(pwd, env);
 	  //	  env = setenv(oldpwd, env);
 	  return (env);
@@ -85,27 +85,49 @@ t_env	*cd_directory(t_env *env, char *directory)
   return (env);
 }
 
+t_env	*cd_go_home(t_env *env)
+{
+  t_env *tmp;
+  char *home;
+  char *oldpwd;
+  char *pwd;
+
+  tmp = env;
+  while (tmp != NULL)
+    {
+      if ((my_nncmp(tmp->value, "HOME=", 0)) == 0)
+	{
+	  home = my_ncpy(tmp->value, 5);
+	  pwd = my_concat("PWD ", home);
+	  chdir(home);
+	}
+      if ((my_nncmp(tmp->value, "PWD=", 0)) == 0)
+	oldpwd = my_concat("OLDPWD ", my_ncpy(tmp->value, 4));
+      tmp = tmp->next;
+    }
+  // env = setenv(oldpwd, env);
+  // env = setenv(pwd, env);
+  my_printf("\n%s\n%s\n", pwd, oldpwd);
+  return (env);
+}
+
 t_env	*shell_cd(t_env *env, char *directory)
 {
-  my_aff_env(env);
+  //  my_aff_env(env);
+  if (directory == NULL)
+    {
+      env = cd_go_home(env);
+      return (env);
+    }
   if ((my_nncmp(directory, "..", 0)) == 0)
     env = cd_prev(env);
+  else if ((my_nncmp(directory, "-L", 0)) == 0)
+    env = cd_go_home(env);
+  else if ((my_nncmp(directory, "-P", 0)) == 0)
+    env = cd_go_home(env);
   else if ((my_nncmp(directory, "-", 0)) == 0)
     env = cd_minus(env);
   else
     env = cd_directory(env, directory);
   return (env);
-}
-
-int	main(int argc, char **argv, char **env)
-{
-  char buffer[5000];
-  t_env *list;
-
-  if (argc == 3)
-    my_printf("%s\nON A PRIS TA MERE EN OTAGE\n", argv[1]);
-  list = my_env_in_list(env);
-  read(0, buffer, 5000);
-  shell_cd(list, buffer);
-  return (0);
 }
--- cd_by_booba/cd.h
+++ cd_by_booba/cd.h
@@ -5,7 +5,7 @@
 ** Login   <auffra_a@epitech.net>
 ** 
 ** Started on  Fri May 23 16:48:28 2014 auffra_a
-** Last update Fri May 23 16:49:16 2014 auffra_a
+** Last update Sun May 25 00:23:07 2014 auffra_a
 */
 
 char    *cd_pwding(t_env *env);
@@ -16,6 +16,7 @@ t_env   *cd_prev(t_env *env);
 t_env   *cd_minus(t_env *env);
 t_env   *cd_directory(t_env *env, char *directory);
 t_env   *shell_cd(t_env *env, char *directory);
+t_env   *cd_go_home(t_env *env);
 
 char    *my_ncpy(char *s, int x1);
 char    *my_concat(char *s1, char *s2);
