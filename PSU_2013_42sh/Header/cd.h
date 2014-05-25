1b8c8a2defdba44fcd2aa4e7263e42e528312509
auffra_a <auffra_a@epitech.eu>
2014-05-25 16:33:54 +0200

    cd newww

--- PSU_2013_42sh/Header/cd.h
+++ PSU_2013_42sh/Header/cd.h
@@ -5,7 +5,7 @@
 ** Login   <auffra_a@epitech.net>
 ** 
 ** Started on  Fri May 23 16:48:28 2014 auffra_a
-** Last update Sat May 24 23:27:45 2014 theven_d
+** Last update Sun May 25 16:33:37 2014 auffra_a
 */
 
 #include <env.h>
@@ -24,5 +24,10 @@ t_env   *shell_cd(t_env *env, char *directory);
 
 char    *my_ncpy(char *s, int x1);
 char    *my_concat_cd(char *s1, char *s2);
+
+char    *get_home(t_env *env);
+t_env   *cd_home(t_env *env, char *directory);
+t_env   *cd_by_home(t_env *env, char *directory);
+t_env   *cd_p(t_env *env, char *directory);
 
 #endif
